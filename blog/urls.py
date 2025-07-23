e7ea8da843a1992ae4e3b2bf6bd8e4aadae4f5d9
yusufsyahputraa <myusuf28062005@gmail.com>
2025-07-23 09:40:07 +0800

    FINAL CHAPTER

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from self import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,6 @@ urlpatterns = [
     path('motivasi/', views.motivasi_view, name='motivasi'),
     path('', include('self.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
