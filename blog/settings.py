e18f68aeb5c52a5cd4133f974cc3f4991de7b4b3
yusufsyahputraa <myusuf28062005@gmail.com>
2025-07-23 09:46:55 +0800

    Terakhir

--- blog/settings.py
+++ blog/settings.py
@@ -114,12 +114,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Pastikan ini sesuai
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Untuk production (deploy)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
